af532aabc6d1e42821fac22ded221073ec3ac7f1
Alex Berber <berber.it@gmail.com>
2021-06-24 15:30:10 +0300

    Bump versions Terraform to 1.0.0 and hashicorp/aws to 3.46.0

--- examples/multi-az/config.tf
+++ examples/multi-az/config.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.15.0"
+  required_version = ">= 1.0.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.70.0"
+      version = ">= 3.46.0"
     }
     random = {
       source  = "hashicorp/random"
